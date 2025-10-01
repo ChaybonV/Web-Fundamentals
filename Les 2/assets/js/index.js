e0c5474e6bbdd02f35d35b7da7cb1cf87dcf483f
CV <84094808+Joqqqq@users.noreply.github.com>
2025-10-01 14:35:33 +0200

    change les 2

--- Les 2/assets/js/index.js
+++ Les 2/assets/js/index.js
@@ -1,138 +1,158 @@
-// // For in ES5 
-// // shortcut: select ctrl + / or cmd + /
-// // for (var i = 0; i < 10; i++) {
-// //     console.log(i);
-// // }
+// ES 5 way 
+// COMMENT OUT -> ctrl+/ of cmd+/
+// var firstname = "Collin";
 
-// // es6 
-// const app_name = "I love collins classes";
-// let first_name = "Collin";
-// first_name = "test";
-// // Want to copy+paste a line? 
-// // shortcut:  option + shift + arrow down / alt + shift + arrow down
-// console.log(app_name); 
-// console.log(first_name); 
-
-// for (let i = 0; i < 10; i++) {
+// for(var i = 0; i < 10; i++) {
 //     console.log(i);
 // }
 
 // console.log(i);
 
-// 2 variables fn ln 
-// log fullname
+// ES6 variable
+// let firstname = "Collin";
+// const appname = "Tinder";
+// var age = 31;
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// console.log(i);
+
 // let fn = "Collin";
 // let ln = "Van der Vorst";
-// let fullname = `${fn} ${ln}`; // f"" $""
-// console.log(fullname);
+// console.log(`${fn} ${ln}`);
 
-// ES5
-// const square = (number) => {
-//     return number * number;
+// ES 5 
+// const square = (getal) => {
+//     return getal * getal;
 // }
-// const square = number => {
-//     return number * number;
-// }
-// const square = number => number * number;
 
 // console.log(square(5));
 
 // let user = {
-//   lastname: "Van der Vorst",
-//   firstname: "Collin",
-//   age: 30
+//     fn: "Collin",
+//     password: "IHateFifa26",
+//     isBlocked: true
 // };
 
-// let {lastname, firstname:fn } = user;
-// console.log(lastname);
-// console.log(fn);
-// console.log(firstname);
+// let {password: pass, isBlocked} = user;
 
-let goats = ["T-rex", "Connie", "Clyde"];
-// let [goats1, goats2, goats3] = goats;
-// console.log(goats1);
-// console.log(goats2);
-// console.log(goats3);
+// console.log(isBlocked);
+// console.log(pass);
 
-let sheeps = ["sheep1", "sheep2", "sheep3", "sheep4"];
-let animals = [...goats, ...sheeps];
+// let goats = ["T-rex", "Connie", "Clyde"];
+// let [goat1, goat2, goat3] = goats;
+// console.log(goat1);
+// console.log(goat2);
+// console.log(goat3);
 
-// let smallest = [];
-// let largest = [];
+// let goats = ["T-rex", "Connie", "Clyde"];
+// let sheeps = ["sheep1", "sheep2", "sheep3", "sheep4"];
 
-// let mixedAnimals = [];
 
-// if (sheeps.length > goats.length) {
-//     largest = sheeps;
-//     smallest = goats;
-// } else {
-//     largest = goats;
-//     smallest = sheeps;
-// }
-
-// for(let i = 0; i < largest.length; i++) {
-//     mixedAnimals.push(largest[i]);
-
-//     if (i < smallest.length) {
-//         mixedAnimals.push(smallest[i]);
+// const mergeArrays = (array1, array2) => {
+//     let mergedArrays = [];
+//     let smallest = [];
+//     let largest = [];
+    
+//     // Bepalen wat de grootste array is
+//     if (array1.length > array2.length) {
+//         largest = array1;
+//         smallest = array2;
+//     } else {
+//         largest = array2;
+//         smallest = array1;
 //     }
+
+//     // Mengen 
+//     for (let i = 0; i < largest.length; i++) {
+//         mergedArrays.push(largest[i]);
+
+//         if (smallest[i] != null) {
+//             mergedArrays.push(smallest[i]);
+//         }
+//     }
+
+//     return mergedArrays;
 // }
 
-// console.log(mixedAnimals);
-// Mix goats and sheeps in a new array called animals
-// .push()
-// let animals = [];
-// for(let i = 0; i < goats.length;i++) {
-//     animals.push(goats[i]);
-// }
-// sheeps.forEach((sheep) => {
-//     animals.push(sheep);
-// });
-
+// let animals = mergeArrays(sheeps, goats);
 // console.log(animals);
 
+
+
+// O: "sheep1", "t-rex", "sheep2", "connie", "sheep3", "clyde", "sheep4"
+
+// for (let i = 0; i < goats.length; i++) {
+//     animals.push(goats[i]);
+// }
+// for (let i = 0; i < sheeps.length; i++) {
+//     animals.push(sheeps[i]);
+// }
+
+// Ik wil een nieuwe array, Animals 
+// Alle goats en alle sheeps 
+// .push()
+// goats.length
+// animals = ["t-rex", "Connie", "clyde", "sheep1" ..]
+
 // class Person {
-//     constructor(_name, _age) {
-//         this.name = _name;
-//         this.age = _age;
+//     constructor (fn, age) {
+//         this.fn = fn;
+//         this.age = age;
 //     }
 
-//     sayYourAge() {
-//         console.log(`I am ${this.age} years old`);
+//     sayAge() {
+//         console.log(`I am ${this.fn}, my age is ${this.age}`);
 //     }
 // }
 
 // class Teacher extends Person {
-//     constructor(_name, _age, _degree) {
-//         super(_name, _age);
-//         this.degree = _degree;
+//     constructor(fn, age, degree) {
+//         super(fn, age);
+//         this.degree = degree;
+//     }
+
+//     teach() {
+//         console.log("teachaaah");
 //     }
 // }
 
-// let person1 = new Person("Collin", 34);
-// person1.sayYourAge();
+// let collin = new Person("collin", 32);
+// collin.sayAge();
 
-let grades = [6, 3, 10, 14, 20];
+// let elke = new Teacher("elke", 0, "master");
+// elke.sayAge();
+// elke.teach();
 
-let gradesOn100 = grades.map(grade => grade / 20 * 100);
-
-let failedGrades = gradesOn100.filter(grade => grade < 50);
+let grades = [7, 14, 8, 6, 19]; // PF 
+let gradesOn100 = grades.map((grade) => {
+    grade = grade / 20 * 100;
+    return grade;
+});
 console.log(gradesOn100);
-console.log(failedGrades);
 
 let users = [
     {
-        fn: "Collin",
-        isBlocked: true
+        id: 1,
+        name: "Collin",
+        isBlocked: false
     },
     {
-        fn: "Lisa",
+        id: 2,
+        name: "Lisa",
+        isBlocked: false
+    },
+    {
+        id: 1,
+        name: "John",
         isBlocked: true
     },
 ];
-users = users.map((user) => {
-    user.isMarried = true;
-    return user;
-})
-let blockedUsers = users.filter(user => user.isBlocked == true);
-console.log(blockedUsers);
+
+let blockedUsers = users.filter((user) => user.isBlocked == true);
+
+blockedUsers.forEach((user) => {
+    console.log(user.name);
+});
