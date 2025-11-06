26dbba0073fea8d629671903768d73b2935e33e4
CV <84094808+Joqqqq@users.noreply.github.com>
2025-11-06 12:28:27 +0100

    add les start vue

---
+++ eurosong-front/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+  // ----- Import
+  import {ref} from "vue";
+
+  // ----- Components
+  import Counter from "./components/Counter.vue"
+
+  // ----- Data
+  let counters = ref([
+    {
+      "name":"Verlooy",
+      "startValue":2
+    },
+    {
+      "name":"Chaybon",
+      "startValue":0
+    },
+    {
+      "name":"Joeri",
+      "startValue":0
+    }
+  ])
+</script>
+
+<template>
+<div>
+  <h1>Counter van domme vragen</h1>
+
+  <div v-for="(item,index) in counters" :key="index">
+    <Counter v-bind:counterName="item.name" :counterInitialValue="item.startValue"/>
+  </div>
+
+</div>
+</template>
---
+++ eurosong-front/src/components/Counter.vue
@@ -0,0 +1,65 @@
+<!-- Settings van de component -->
+
+<script setup>
+  // ----- Import
+  import {ref,onMounted} from "vue"; // ref gebruiken we als we data willen linken met de template
+
+  // ----- Props -> gebruiken we als onze parent data moet geven, READ ONLY
+  const props = defineProps({
+    counterName: String,
+    counterInitialValue: Number
+  })
+
+  // ----- Data -> gebruiken we intern en hierbij kunnen we data aanpassen
+  // const counterName = ref("Lukas"); --> in comment omdat het een prop is en dus niet meer nodig is hier
+  let counterValue = ref(0);
+
+  // ----- Lifecycles
+  onMounted(()=>{
+    counterValue.value = props.counterInitialValue;
+  })
+
+  // ----- Methods
+  const increment = ()=>{
+    counterValue.value++;
+    // console.log("Auwtch, you clicked the button");
+  }
+
+  const decrement = ()=>{
+    if(counterValue.value>0){
+        counterValue.value--;
+    }
+  }
+
+  const showMinButton = ()=>{
+    if(counterValue.value>0){
+        return true;
+    }
+    else{
+        return false;
+    }
+  }
+</script>
+
+<!-- Wat de gebruiker zal zien en is gelinkt met script -->
+
+<template>
+    <div>
+        <h2>
+            {{ counterName }}
+        </h2>
+        <p>
+            {{ counterValue }}
+        </p>
+
+        <!-- You can use v-on:click, but we use shorthand @click -->
+        <button @click="increment">
+            Add
+        </button>
+        <br>
+        <!-- v-if="showMinButton()" -> use if you want to hide/show button based on value  -->
+        <button @click="decrement" :disabled="counterValue<=0">
+            Min
+        </button>
+    </div>
+</template>
